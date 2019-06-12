35518ee173b523c620a71241e40e711cd151cbb9
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2019-06-12 14:32:45 +0300

    unimportant

--- csrc/luasec/build.sh
+++ csrc/luasec/build.sh
@@ -1,3 +1,4 @@
+mkdir -p "$(dirname ../../bin/$P/clib)"
 ${X}gcc -c -O2 $C $files -Isrc -I../lua-headers -I../openssl/include
 ${X}gcc *.o -shared -o ../../bin/$P/clib/$D $L
 rm -f      ../../bin/$P/$A
