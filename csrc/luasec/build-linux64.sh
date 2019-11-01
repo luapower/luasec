73f8381f56576766a61d91cc6210ef5b829686ee
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2019-11-01 08:43:40 -0400

    unimportant

--- csrc/luasec/build-linux64.sh
+++ csrc/luasec/build-linux64.sh
@@ -3,7 +3,7 @@ mkdir -p luasocket
 cp -f $LS/*.h luasocket/
 files="config.c context.c ec.c ssl.c x509.c
 	$LS/buffer.c $LS/io.c $LS/timeout.c $LS/usocket.c" \
-	P=linux64 C="-DWITH_LUASOCKET" \
+	P=linux64 C="-fPIC -DWITH_LUASOCKET" \
 	L="-s -static-libgcc -lluajit" \
 	D=ssl.so A=ssl.a ./build.sh
 rm -rf luasocket
