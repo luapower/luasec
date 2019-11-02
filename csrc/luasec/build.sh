ba3085cf3490ba2742c44fcd7e946b790aa72948
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2019-11-02 02:43:45 +0200

    unimportant

--- csrc/luasec/build.sh
+++ csrc/luasec/build.sh
@@ -6,7 +6,6 @@ mkdir -p "$(dirname ../../bin/$P/clib)"
 
 mkdir -p luasocket
 cp -f ../socket/src/*.{h,c} luasocket/
-(cd luasocket && cat ../*.patch | patch -N -p1)
 
 ${X}gcc -c -O2 $C $files -I. -I../lua-headers -I../openssl/src/include
 ${X}gcc *.o -shared -o ../../bin/$P/clib/$D -L../../bin/$P -lssl -lcrypto $L
