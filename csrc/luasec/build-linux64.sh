f0e88cf7b60545967b109ae9c9438ef1788fc937
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2019-11-03 08:58:43 +0200

    unimportant

--- csrc/luasec/build-linux64.sh
+++ csrc/luasec/build-linux64.sh
@@ -1,6 +1,6 @@
-cp -f ../openssl/opensslconf.h.linux64 ../openssl/src/include/openssl/opensslconf.h
-files="config.c context.c ec.c ssl.c x509.c
-	luasocket/buffer.c luasocket/io.c luasocket/timeout.c luasocket/usocket.c" \
-	P=linux64 C="-fPIC -DWITH_LUASOCKET" \
-	L="-s -static-libgcc -lluajit" \
-	D=ssl.so A=ssl.a ./build.sh
+P=linux64 C="
+    -fPIC -DWITH_LUASOCKET
+    -I../openssl/include-linux64
+    config.c context.c ec.c ssl.c x509.c
+    luasocket/buffer.c luasocket/io.c luasocket/timeout.c luasocket/usocket.c
+" L="-s -static-libgcc -lluajit" D=ssl.so A=ssl.a ./build.sh
--- csrc/luasec/build.sh
+++ csrc/luasec/build.sh
@@ -7,7 +7,7 @@ mkdir -p "$(dirname ../../bin/$P/clib)"
 mkdir -p luasocket
 cp -f ../socket/src/*.{h,c} luasocket/
 
-${X}gcc -c -O2 $C $files -I. -I../lua-headers -I../openssl/src/include
+${X}gcc -c -O2 $C -I. -I../lua-headers -I../openssl/src/include
 ${X}gcc *.o -shared -o ../../bin/$P/clib/$D -L../../bin/$P -lssl -lcrypto $L
 rm -f      ../../bin/$P/$A
 ${X}ar rcs ../../bin/$P/$A *.o
